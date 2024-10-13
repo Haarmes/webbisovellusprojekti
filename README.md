667d962c52ddc6b6f70038b048c10453d9876c49
Haarmes <Sevenposti@hotmail.com>
2024-10-13 21:33:05 +0300

    catFacts

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
 # webbisovellusprojekti
  
+HTML (25%)
+1/5:
+---Basic HTML structure is present.
+
+2/5:
+HTML structure with clear content differentiation (headings, paragraphs, lists).
+
+3/5:
+Use of forms, links, and media.
+
+4/5:
+Tables are effectively used.
+
+5/5:
+Consistent use of semantic HTML throughout, ensuring better structure and understanding of the content.
+
+
+CSS (25%)
+1/5:
+Basic CSS styling (colors, fonts).
+
+2/5:
+Use of classes and IDs to style specific elements.
+
+3/5:
+Implementation of responsive design elements.
+
+4/5:
+Use of layouts for advanced user interfaces (arrays, float, flexbox, css grid)
+
+5/5:
+Styling demonstrates a strong grasp of layout principles, aesthetics, and user experience.
+
+
+JavaScript Basics (25%)
+1/5:
+Simple interactions (like alerts on button click).
+
+Learn about JS Alerts
+2/5:
+Multiple event listeners and basic DOM manipulations.
+
+Learn about Event Listeners
+3/5:
+Use of arrays, objects, and functions.
+
+4/5:
+Advanced logic, looping through data, and dynamic DOM updates.
+
+5/5:
+Consistent use of Object-Oriented JavaScript principles.
+
+
+Asynchronous Operations (25%)
+1/5:
+Use of timers.
+
+Learn about Timers
+2/5:
+Successful implementation of an AJAX call or Fetch.
+
+3/5:
+Data from the asynchronous call is displayed on the webpage.
+
+4/5:
+Error handling is implemented (for failed API calls, etc.).
+
+Learn about JS Error Handling
+5/5:
+Effective use of asynchronous data to enhance user experience (like filtering, sorting).
--- js/script.js
+++ js/script.js
@@ -1,2 +1,78 @@
-let d = new Date();
-document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
+//let d = new Date();
+//document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
+let geoLatitude, geoLongitude;
+let geoApiKey = "d594093470b3d6bc2814a1178318feec";
+let kelvinTransfer = 273.15;
+const link1 = document.getElementById("link1ID");
+const link2 = document.getElementById("link2ID");
+const link3 = document.getElementById("link3ID");
+
+link1.addEventListener("click", function () {
+    //alert("link1");
+    window.open("https://github.com/haarmes", '_blank').focus();
+})
+link2.addEventListener("click", function () {
+    //alert("link1");
+    window.open("https://www.youtube.com/Haarmes", '_blank').focus();
+})
+link3.addEventListener("click", function () {
+    //alert("link1");
+    window.open("https://www.twitch.tv/haarmes", '_blank').focus();
+})
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        geoLatitude = position.coords.latitude;
+        geoLongitude = position.coords.longitude;
+        console.log(`Latitude: ${geoLatitude}, Longitude: ${geoLongitude}`);
+    });
+} else {
+    console.log("Geolocation is not supported by this browser.");
+}
+
+function getWeatherInfo() {
+    // Define the API URL
+    //const apiU = 'http://api.openweathermap.org/geo/1.0/direct?q=';
+    const apiUrl2 = 'https://api.openweathermap.org/data/2.5/weather?lat=' + geoLatitude + "&lon=" + geoLongitude + "&appid=" + geoApiKey;
+
+    console.log(apiUrl2);
+    // Make a GET request
+    fetch(apiUrl2)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            let tempCels = Number(data.main.temp) - kelvinTransfer;
+            console.log("tempcels = " + tempCels);
+            console.log(data.main.temp);
+            document.getElementById("weatherTemp").innerHTML = "Current temperature is: " + parseInt(tempCels) + "°C";
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
+function show(shown, hidden) {
+
+    document.getElementById(shown).style.display = 'flex';
+    document.getElementById(hidden).style.display = 'none';
+
+    return false;
+
+}
+
+async function clickFunctionText() {
+    console.log("yes1");
+    let text = document.getElementById("textBooks").innerHTML;
+    console.log("yes2");
+    //const response = await fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + text + '&limit=1&appid=' + geoApiKey);
+    const response = await fetch('https://cat-fact.herokuapp.com/fact');
+    console.log("yes");
+    console.log(response);
+    document.getElementById("textBooks").value = "";
+    document.getElementById("textInput").innerHTML = "You send a text with " + text;
+}
